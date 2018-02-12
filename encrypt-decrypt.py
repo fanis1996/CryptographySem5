f16bf3309321e089a141efec58cddb7b3cf5fbdc
Julio B <julio.bacel@gmail.com>
2018-02-13 01:48:21 +0200

    Code cleanup

--- encrypt-decrypt.py
+++ encrypt-decrypt.py
@@ -20,42 +20,37 @@ def parse_args(argv):
 
 if __name__ == "__main__":
     args = parse_args(sys.argv[1:])
+
+    ciopts = {
+              "AES":{"cipher": AES,
+                     "key":    Padding.pad(args.key.encode(), 32),
+                     "mode":   AES.MODE_CBC,
+                     "pad":    32,
+                     "iv" :    16},
+              "DES":{"cipher": DES,
+                     "key":    Padding.pad(args.key.encode(), 8),
+                     "mode":   DES.MODE_CBC,
+                     "pad":    8,
+                     "iv" :    8},
+             "CAST":{"cipher": CAST,
+                     "key":    Padding.pad(args.key.encode(), 16),
+                     "mode":   CAST.MODE_CBC,
+                     "pad":    8,
+                     "iv" :    8},
+        "Blowfish": {"cipher": Blowfish,
+                     "key":    args.key.encode(),
+                     "mode":   CAST.MODE_CBC,
+                     "pad":    8,
+                     "iv" :    8},
+        }
+
+    c = ciopts[args.cipher]
+    iv = None if args.encrypt else args.infile.read(c['iv'])
+    cipher = c["cipher"].new(c["key"], c["mode"], iv)
+
     if args.encrypt:
-        if args.cipher == 'AES':
-            cipher = AES.new(Padding.pad(args.key.encode(),32),AES.MODE_CBC)
-            cryptedtext=cipher.iv+cipher.encrypt(Padding.pad(args.infile.read(),32))
-            args.outfile.write(cryptedtext)
-        elif args.cipher == 'DES':
-            cipher = DES.new(Padding.pad(args.key.encode(),8),DES.MODE_CBC)
-            cryptedtext=cipher.iv+cipher.encrypt(Padding.pad(args.infile.read(),8))
-            args.outfile.write(cryptedtext)
-        elif args.cipher == 'CAST':
-            cipher = CAST.new(Padding.pad(args.key.encode(),16),CAST.MODE_CBC)
-            cryptedtext=cipher.iv+cipher.encrypt(Padding.pad(args.infile.read(),8))
-            args.outfile.write(cryptedtext)
-        elif args.cipher == 'Blowfish':
-            cipher = Blowfish.new(args.key.encode(),CAST.MODE_CBC)
-            cryptedtext=cipher.iv+cipher.encrypt(Padding.pad(args.infile.read(),8))
-            args.outfile.write(cryptedtext)
-            
+        cryptedtext=cipher.iv+cipher.encrypt(Padding.pad(args.infile.read(), c["pad"]))
+        args.outfile.write(cryptedtext)
     else:
-        if args.cipher == 'AES':
-            iv=args.infile.read(16)
-            cipher = AES.new(Padding.pad(args.key.encode(),32),AES.MODE_CBC,iv)
-            text=cipher.decrypt(args.infile.read())
-            args.outfile.write(Padding.unpad(text,32))
-        elif args.cipher == 'DES':
-            iv=args.infile.read(8)
-            cipher = DES.new(Padding.pad(args.key.encode(),8),AES.MODE_CBC,iv)
-            text=cipher.decrypt(args.infile.read())
-            args.outfile.write(Padding.unpad(text,8))
-        elif args.cipher == 'CAST':
-            iv=args.infile.read(8)
-            cipher = CAST.new(Padding.pad(args.key.encode(),16),AES.MODE_CBC,iv)
-            text=cipher.decrypt(args.infile.read())
-            args.outfile.write(Padding.unpad(text,8))
-        elif args.cipher == 'Blowfish':
-            iv=args.infile.read(8)
-            cipher = Blowfish.new(args.key.encode(),CAST.MODE_CBC,iv)
-            text=cipher.decrypt(args.infile.read())
-            args.outfile.write(Padding.unpad(text,8))
+        text=cipher.decrypt(args.infile.read())
+        args.outfile.write(Padding.unpad(text, c["pad"]))
