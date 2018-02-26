c7a8798f2ffa4f03dddb0c6d44ad6165bd57cf61
fanis1996 <fanis1996GR20@hotmail.com>
2018-02-26 19:57:10 +0200

    Add files via upload

--- hex_edit.py
+++ hex_edit.py
@@ -1,6 +1,7 @@
 import curses
 import binascii
 import os
+import sys
 import math
 from curses import ascii
 
@@ -10,28 +11,91 @@ __HEX_PAD_W = __BLOCK_S * 3
 __BYTE_PAD_W = __BLOCK_S
 
 def hex_edit(filepath):
+    x = 0
+    y = 1
     with open(filepath,'rb') as f:
         data = f.read()
     rows = math.ceil(len(data)/ __BLOCK_S)
     Trows, Tcolumns = [int(s) for s in os.popen('stty size', 'r').read().split()]
     scr = curses.initscr()
+    curses.noecho()
     offsetdisplay = curses.newpad(Trows,__OFFSET_PAD_W)
     hexdisplay = curses.newpad(Trows, __HEX_PAD_W)
-    bytedisplay = curses.newpad(Trows, __BYTE_PAD_W)
+    asciidisplay = curses.newpad(Trows, __BYTE_PAD_W)
+    asciidisplay.keypad(True)
+    asciidisplay.idlok(True)
+    asciidisplay.scrollok(True)
     draw_offs_display(offsetdisplay,rows)
     draw_hex_display(hexdisplay,data)
-    draw_byte_display(bytedisplay,data)
+    draw_byte_display(asciidisplay,data)
+    curses.doupdate()
     while 1:
-        c = bytedisplay.getch()
+        c = asciidisplay.getch()
         if c == ord('q'):
+            #save and exit
+            with open(filepath,'wb') as f:
+                f.write(data)
             break
+        elif c == curses.KEY_RIGHT and x<__BYTE_PAD_W-1:
+            hexdisplay.chgat(y,3*x,2,)
+            x+=1
+        elif c == curses.KEY_LEFT and x>0:
+            hexdisplay.chgat(y,3*x,2,)
+            x-=1
+        elif c == curses.KEY_UP and y>1:
+            hexdisplay.chgat(y,3*x,2,)
+            y-=1
+        elif c == curses.KEY_DOWN and y<Trows-1:
+            hexdisplay.chgat(y,3*x,2,)
+            y+=1
+        elif c == ord('e'):
+            #edit
+            bitsdisplay = curses.newwin(1,9,0,16)
+            bitsdisplay.keypad(True)
+            bitsdisplay.addstr('{0:08b}'.format(data[x+(y-1)*__BLOCK_S]))
+            bitsdisplay.move(0,0)
+            bitsdisplay.refresh()
+            xb = 0
+            while 1:
+                c = bitsdisplay.getch()
+                if c == ord('e'):
+                    break
+                elif c == 'a':
+                    #flip bit
+                    bitsdisplay.erase()
+                    
+                    data[x+(y-1)*__BLOCK_S] ^=1<<(7-xb)
+                    scr.addstr(0,40,data[x+(y-1)*__BLOCK_S])
+                    scr.refresh()
+                    bitsdisplay.addstr('{0:08b}'.format(data[x+(y-1)*__BLOCK_S]))
+                    bitsdisplay.refresh()
+                elif c == curses.KEY_LEFT and xb>0:
+                    #move left
+                    xb-=1
+                elif c == curses.KEY_RIGHT and xb<8:
+                    #move right
+                    xb+=1
+                bitsdisplay.move(0,xb)
+                bitsdisplay.refresh()
+            bitsdisplay.erase()
+            bitsdisplay.refresh()
+                    
+
+            
+        hexdisplay.chgat(y,3*x,2,curses.A_STANDOUT)
+        hexdisplay.noutrefresh(0,0,1, __OFFSET_PAD_W + 2,30, __OFFSET_PAD_W + 2 + __HEX_PAD_W)
+        asciidisplay.move(y,x)
+        asciidisplay.noutrefresh(0,0,1,__OFFSET_PAD_W + 2 + __HEX_PAD_W + 2, 30, __OFFSET_PAD_W +2 + __HEX_PAD_W + 2 + __BYTE_PAD_W)
+        curses.doupdate()
+        
+        
     curses.endwin()
 
 def draw_offs_display(offsd,rows):
     offsd.addstr("- offset -")
     for i in range(rows):
         offsd.addstr("{0:#0{1}x}".format(i * __BLOCK_S,10))
-    offsd.refresh(0,0,1,0,30, __OFFSET_PAD_W)
+    offsd.noutrefresh(0,0,1,0,30, __OFFSET_PAD_W)
 
 def draw_hex_display(hexd,data):
     for i in range( __BLOCK_S):
@@ -39,16 +103,17 @@ def draw_hex_display(hexd,data):
         #https://stackoverflow.com/questions/19210414/byte-array-to-hex-string
     hexdata = binascii.hexlify(data).decode('ascii')
     hexd.addstr(' '.join(hexdata[i:i+2] for i in range(0, len(hexdata), 2)))
-    hexd.refresh(0,0,1, __OFFSET_PAD_W + 2,30, __OFFSET_PAD_W + 2 + __HEX_PAD_W)
+    hexd.noutrefresh(0,0,1, __OFFSET_PAD_W + 2,30, __OFFSET_PAD_W + 2 + __HEX_PAD_W)
 
-def draw_byte_display(byted,data):
+def draw_byte_display(asciidisplay,data):
     for  i in range(__BLOCK_S):
-        byted.addstr("{0:X}".format(i))
+        asciidisplay.addstr("{0:X}".format(i))
     for i in range(len(data)):
         if(ascii.isgraph(data[i])):
-            byted.addch(data[i])
+            asciidisplay.addch(data[i])
         else:
-            byted.addch('.')
-    byted.refresh(0,0,1,__OFFSET_PAD_W + 2 + __HEX_PAD_W + 2, 30, __OFFSET_PAD_W +2 + __HEX_PAD_W + 2 + __BYTE_PAD_W)
+            asciidisplay.addch('.')
+    asciidisplay.move(0,0)
+    asciidisplay.noutrefresh(0,0,1,__OFFSET_PAD_W + 2 + __HEX_PAD_W + 2, 30, __OFFSET_PAD_W +2 + __HEX_PAD_W + 2 + __BYTE_PAD_W)
 
-hex_edit('/tmp/tete')
+hex_edit(sys.argv[1])
